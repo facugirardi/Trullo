834734ad64b4d26054a1d1ca1b9457f254c874f7
facugirardi <you@example.com>
2022-11-10 10:35:45 -0300

    entrega 2

---
+++ js/log.js
@@ -0,0 +1,35 @@
+let log2 = false;
+
+function log(){
+
+    log2 = true;
+    if(log2){
+        document.getElementById('buttons2').style.display = 'none';
+        document.getElementById('buttons1').style.display = 'block';
+    } else{
+        document.getElementById('buttons1').style.display = 'none';
+        document.getElementById('buttons2').style.display = 'block';
+    }
+
+}
+
+function ulog(){
+    log2 = false;
+    if(log2){
+        document.getElementById('buttons2').style.display = 'none';
+        document.getElementById('buttons1').style.display = 'block';
+    } else{
+        document.getElementById('buttons1').style.display = 'none';
+        document.getElementById('buttons2').style.display = 'block';
+    }
+
+}
+
+
+if(log2){
+        document.getElementById('buttons2').style.display = 'none';
+        document.getElementById('buttons1').style.display = 'block';
+} else{
+    document.getElementById('buttons1').style.display = 'none';
+    document.getElementById('buttons2').style.display = 'block';
+}
